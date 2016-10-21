e5c649a0066c5fa6b3994a7b665ae3ba97116d2d
pdziekan <pdziekanp@gmail.com>
2016-10-21 12:03:03 +0200

    Revert "no buoyancy smoothing"
    This reverts commit 1e78ddd28dc9a7bdd8df96e69a169679839645e8.

--- src/forcings.hpp
+++ src/forcings.hpp
@@ -11,7 +11,7 @@ void slvr_lgrngn<ct_params_t>::buoyancy(typename parent_t::arr_t &th, typename p
   namespace moist_air = libcloudphxx::common::moist_air;
   const real_t eps = moist_air::R_v<real_t>() / moist_air::R_d<real_t>() - 1.;
   tmp1(ijk).reindex(this->zero) = (libcloudphxx::common::earth::g<setup::real_t>() / si::metres_per_second_squared) * ((th(ijk).reindex(this->zero) - (*params.th_e)(this->vert_idx)) / (*params.th_ref)(this->vert_idx));
-  F(ijk) = tmp1(ijk);
-//  this->smooth(tmp1, F);
+
+  this->smooth(tmp1, F);
 }
 
